61ab2ea1b1f1e38f3a8923adec3fdd918cf63b0e
radilr <radil@fandrop.com>
2014-05-06 21:39:49 +0300

    fix analytics

--- recipes/unicorn.rb
+++ recipes/unicorn.rb
@@ -61,8 +61,8 @@ unicorn_config "#{app.config_path}/unicorn.rb" do
       ActiveRecord::Base.establish_connection
     end
 
-    if defined?(Buzzstarter) && Buzzstarter::Config.segment_io_tracking && defined?(Analytics)
-      Analytics.init(secret: Buzzstarter::Config.segment_io_secret)
+    if defined?(#{app.name.camelize}) && #{app.name.camelize}::Config.segment_io_tracking && defined?(AnalyticsRuby)
+      AnalyticsRuby.init(secret: #{app.name.camelize}::Config.segment_io_secret)
     end
 
     worker_pidfile = server.config[:pid].sub('.pid', ".\#{worker.nr}.pid")
