08a38d2689d3c51dcffb81c48a8f47d59761aa6c
Sava Gerov <sava.jg@gmail.com>
2016-02-10 17:54:08 +0100

    cleanup

--- metadata.rb
+++ metadata.rb
@@ -11,9 +11,10 @@ recipe           "webapp", "Default webapp recipe"
 depends 'nginx', '2.2.0' # Fixed at 2.2.0 release since we're overriding it a bit
 depends 'nodejs', '~> 1.3.0'
 depends 'postgresql', '~> 4.0.0'
-depends 'redisio', '~> 1.7.0'
+depends 'redisio', '~> 2.1.0'
 
 # Note: These deps come from Github, and must be imported via your Cheffile (versions fixed to
 # ensure Github version is used or a warning is thrown)
-depends 'rvm', '~> 0.9.1' # must use version from github: 'fnichol/chef-rvm'
-depends 'monit', '~> 1.5.0' # must use version from github: 'phlipper/chef-monit'
+depends 'rvm',     github: 'fnichol/chef-rvm' # must use version from github: 'fnichol/chef-rvm'
+depends 'monit',   github: 'phlipper/chef-monit' # must use version from github: 'phlipper/chef-monit'
+depends 'unicorn', github: 'AmoebaLabs/chef-unicorn'
